866def998e62861a5a80a7e1df1d0c844b40f3b2
Harrison Oglesby <oglesby.harrison@gmail.com>
2025-11-05 21:38:38 -0800

    Add ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,19 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ktlint) apply false
     kotlin("kapt") version libs.versions.kotlin.get() apply false
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        android.set(true)
+        ignoreFailures.set(false)
+        reporters {
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
+        }
+    }
 }
