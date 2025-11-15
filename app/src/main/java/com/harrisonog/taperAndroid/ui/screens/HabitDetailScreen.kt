3729b4e7f4dc8aaf0a20426856b5f0dd81629557
Harrison Oglesby <oglesby.harrison@gmail.com>
2025-11-15 14:09:06 -0800

    fix deprecated tabrow

--- app/src/main/java/com/harrisonog/taperAndroid/ui/screens/HabitDetailScreen.kt
+++ app/src/main/java/com/harrisonog/taperAndroid/ui/screens/HabitDetailScreen.kt
@@ -33,8 +33,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SecondaryTabRow
 import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -109,7 +109,7 @@ fun HabitDetailScreen(
 
         Column(Modifier.fillMaxSize().padding(pad)) {
             // Tab strip
-            TabRow(
+            SecondaryTabRow(
                 selectedTabIndex = pagerState.currentPage,
                 modifier = Modifier.fillMaxWidth()
             ) {
