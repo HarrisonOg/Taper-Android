a976a840739c4528865eeb0bdb021f8c6fcb796a
Harrison Oglesby <oglesby.harrison@gmail.com>
2025-11-24 08:58:59 -0800

    final old macbook push

--- app/src/main/java/com/harrisonog/taperAndroid/ui/components/TaperProgressGraph.kt
+++ app/src/main/java/com/harrisonog/taperAndroid/ui/components/TaperProgressGraph.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.unit.dp
